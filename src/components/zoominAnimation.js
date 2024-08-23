530b8886f2c1dcd4f1ec1f43768aaa3ff19e531f
ztam0002 <zactam60@gmail.com>
2024-08-23 14:23:54 +1000

    Zoom in transition
    Zoom in transition for home page

---
+++ src/components/zoominAnimation.js
@@ -0,0 +1,27 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#hero",
+      start: '2%',
+      end: '+=105%',
+      markers: true,
+      scrub: true,
+      toggleActions: "play reset reverse reverse",
+    },
+  });
+
+  tl.to("#hero h2", {
+    scale: 500,
+    duration: 0.8,
+  });
+
+  tl.to("#hero h2", {
+    opacity: 0,
+    duration: 0.1,
+  });
+});
